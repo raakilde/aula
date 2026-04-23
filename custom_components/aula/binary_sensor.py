709b8fe98d0fc3921ef15579f329bbaa9643822a
Raakilde <j.olesen@vindinggaard.dk>
2026-04-23 23:13:29 +0000

    feat: Check mail every 5 min

--- custom_components/aula/binary_sensor.py
+++ custom_components/aula/binary_sensor.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import timedelta
 
 from homeassistant import config_entries, core
 from homeassistant.components.binary_sensor import BinarySensorEntity
@@ -6,6 +7,9 @@ from homeassistant.components.binary_sensor import BinarySensorEntity
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
+
+# Avoid default 30s polling; align with coordinator refresh cadence.
+SCAN_INTERVAL = timedelta(minutes=30)
 
 
 async def async_setup_entry(
@@ -53,6 +57,9 @@ class AulaBinarySensor(BinarySensorEntity):
         self._text = text
         self._sender = sender
         self._client = self._hass.data[DOMAIN]["client"]
+        # Keep internal binary state stable from startup.
+        self._state = 1 if unread == 1 else 0
+        self._last_logged_state = None
 
     @property
     def extra_state_attributes(self):
@@ -81,14 +88,18 @@ class AulaBinarySensor(BinarySensorEntity):
 
     def update(self):
         if self._client.unread_messages == 1:
-            _LOGGER.debug("There are unread message(s)")
+            if self._last_logged_state != 1:
+                _LOGGER.debug("There are unread message(s)")
+                self._last_logged_state = 1
             # _LOGGER.debug("Latest message: "+str(self._client.message))
             self._subject = self._client.message["subject"]
             self._text = self._client.message["text"]
             self._sender = self._client.message["sender"]
             self._state = 1
         else:
-            _LOGGER.debug("There are NO unread messages")
+            if self._last_logged_state != 0:
+                _LOGGER.debug("There are NO unread messages")
+                self._last_logged_state = 0
             self._state = 0
             self._subject = ""
             self._text = ""
